de7c92e6f4add696be79078276c0a94d158e897d
santi-gdc <santi.gdc17@outlook.com>
2026-06-20 21:44:23 -0600

    admin page routing and example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GuestPage from './components/guest/GuestPage'
+import AdminPage from './pages/admin/AdminPage'
 
 export default function App() {
-  return <GuestPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GuestPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/pages/admin/AdminPage.jsx
@@ -0,0 +1,9 @@
+import { AuthTester } from '../../components/AuthTester'
+
+export default function AdminPage() {
+  return (
+    <div style={{ maxWidth: 520, margin: '0 auto', padding: '40px 24px' }}>
+      <AuthTester />
+    </div>
+  )
+}
